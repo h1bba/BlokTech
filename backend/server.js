15fbc4f051ab7a83de61d7ad309e4453de0e9721
HibbaYT <hibbayt@gmail.com>
2023-02-17 01:09:38 +0100

    Updated, routing toegevoegd, static public map en 404 error screen

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,31 @@
-const express = require('express')
+const express = require("express");
 const http = require("http");
-const app = express()
+const app = express();
 
-const host = 'localhost';
-const port = 8000
+const host = "localhost";
+const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// routing
+app.get("/liked", (req, res) => {
+  res.render("liked.ejs", { data: port });
+});
+
+// app.get("*", (req, res) => {
+//   res.render("404.ejs", { data: port });
+// });
+
+// 404 error
+app.use(function (req, res) {
+  res.status(404).render("404.ejs");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
